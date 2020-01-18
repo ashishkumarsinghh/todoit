249e9dcb685e5f3b4df93398ad32947e6967e7f5
Ashish Kumar Singh <ashishkumarsinghh@outlook.com>
2020-01-18 10:20:45 +0530

    Accept input with enter key press also.

--- src/NewTodoItem.jsx
+++ src/NewTodoItem.jsx
@@ -4,11 +4,28 @@ export default function NewTodoItem(props) {
   const [state, setstate] = useState("");
   const handler = e => {
     e.preventDefault();
-    if (state.length > 0) props.handler(state);
-    setstate("");
+    if (state.length > 0) {
+      props.handler(state);
+      setstate("");
+    }
+  };
+  const handleKeyPress = e => {
+    if (e.charCode === 13) {
+      if (state.length > 0) {
+        props.handler(state);
+        setstate("");
+      }
+    }
+  };
+  const submitHandler = form => {
+    form.preventDefault();
   };
   return (
-    <form className="todoitem newTodoItem" id="todoform">
+    <form
+      className="todoitem newTodoItem"
+      id="todoform"
+      onSubmit={submitHandler}
+    >
       <div className="input-group mb-3">
         <input
           type="text"
@@ -19,6 +36,7 @@ export default function NewTodoItem(props) {
             setstate(e.target.value);
           }}
           value={state}
+          onKeyPress={handleKeyPress}
         />
         <div className="input-group-append">
           <button
